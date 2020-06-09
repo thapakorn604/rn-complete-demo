8faabdc6123b58bcb349563d674b2f4d98ee972e
Thapakorn Tuwaemuesa <thapakorn604@gmail.com>
2020-06-09 14:14:01 +0700

    add useReducer to BlogContext

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,9 +1,54 @@
-import React, { createContext } from 'react'
+import React, { createContext, useReducer } from 'react'
 
 const BlogContext = createContext()
 
+const ADD_BLOGPOST = 'ADD_BLOGPOST'
+const EDIT_BLOGPOST = 'EDIT_BLOGPOST'
+const DELETE_BLOGPOST = 'DELETE_BLOGPOST'
+
+const blogReducer = (state, { type, payload }) => {
+  switch (type) {
+    case ADD_BLOGPOST:
+      return [...state, { title: `Blog #${state.length + 1}` }]
+    default:
+      return state
+  }
+}
+
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider value={5}>{children}</BlogContext.Provider>
+  const [blogPosts, dispatch] = useReducer(blogReducer, [])
+
+  const addBlogPost = () => {
+    dispatch({ type: ADD_BLOGPOST })
+  }
+
+  const editBlogPost = () => {
+    dispatch({ type: EDIT_BLOGPOST })
+  }
+
+  const deleteBlogPost = () => {
+    dispatch({ type: DELETE_BLOGPOST })
+  }
+
+  return (
+    <BlogContext.Provider
+      value={{
+        blogPosts: blogPosts,
+        addBlogPost,
+        editBlogPost,
+        deleteBlogPost,
+      }}>
+      {children}
+    </BlogContext.Provider>
+  )
 }
 
 export default BlogContext
+
+// const initialBlogPosts = [
+//   { title: 'Blog #1' },
+//   { title: 'Blog #2' },
+//   { title: 'Blog #3' },
+//   { title: 'Blog #4' },
+//   { title: 'Blog #5' },
+// ]
--- blog/src/screens/HomeScreen.jsx
+++ blog/src/screens/HomeScreen.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React, { useContext } from 'react'
+import { Text, View, StyleSheet, FlatList, Button } from 'react-native'
+import BlogContext from '../context/BlogContext'
 
-const HomeScreen = ({ params }) => (
-  <View>
-    <Text>HomeScreen</Text>
-  </View>
-)
+const HomeScreen = () => {
+  const { blogPosts, addBlogPost } = useContext(BlogContext)
+
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+      <Button title="Post" onPress={addBlogPost} />
+      <FlatList
+        data={blogPosts}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>
+        }}
+      />
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({})
 
